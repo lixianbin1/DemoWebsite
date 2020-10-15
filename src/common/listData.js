24c1a1a8cf8f203338c71ef86244ce684002cf79
xianbin <1169411465@qq.com>
2020-10-15 17:53:55 +0800

    update:修改demo项目路径

--- src/common/listData.js
+++ src/common/listData.js
@@ -1,20 +1,20 @@
 export const list=[{
-    href:'/Demo/ThreeJS/index.html',
+    href:'./Demo/ThreeJS/index.html',
     img:require('../Static/img/article/1905091.png'),
     title:'three.js',
     text:'three.js制作的演示例子'
 },{
-    href:'/Demo/ReactNote/index.html',
+    href:'./Demo/ReactNote/index.html',
     img:require('../Static/img/article/18344.jpg'),
     title:'Hooks',
     text:'React 16.8版本采用Hooks新特性创建的备忘录'
 },{
-    href:'/Demo/Lacking/index.html',
+    href:'./Demo/Lacking/index.html',
     img:require('../Static/img/article/1906111.jpg'),
     title:'Lacking',
     text:'React 组件例子'
 },{
-    href:'/Demo/BigScreen/index.html',
+    href:'./Demo/BigScreen/index.html',
     img:require('../Static/img/article/1906111.jpg'),
     title:'Echart图例展示',
     text:'Echarts的分辨率兼容（去除了敏感性东西，可能显的空）'
